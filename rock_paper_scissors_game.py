5bcd20707708039311e8fa8bf4e13c6ca250e8bc
Tege1337 <toth-nador.tege@diak.szbi-pg.hu>
2024-11-25 09:33:40 +0100

    done

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -2,4 +2,30 @@ import random
 
 choices = ["rock", "paper", "scissors"]
 
-# machine_choice = random.choice(choices)
+while True:
+        
+    user_choice = input("Rock, Paper or Scissors?: ").lower()
+    opponent_choice = random.choice(choices).lower()
+
+    a = user_choice
+    b = opponent_choice
+
+    # Tie
+    if a == b:
+        print(f"It's a tie! You both chose {a}")
+
+    # Win
+    if a == "SCISSORS" and b == "PAPER":
+        print(f"You won! You chose {a}, and the opponent chose {b}!")
+    if a == "PAPER" and b == "ROCK":
+        print(f"You won! You chose {a}, and the opponent chose {b}!")
+    if a == "ROCK" and b == "SCISSORS":
+        print(f"You won! You chose {a}, and the opponent chose {b}!")
+
+    # Lose
+    if b == "SCISSORS" and a == "PAPER":
+        print(f"You lost! You chose {a}, and the opponent chose {b}!")
+    if b == "PAPER" and a == "ROCK":
+        print(f"You lost! You chose {a}, and the opponent chose {b}!")
+    if b == "ROCK" and a == "SCISSORS":
+        print(f"You lost! You chose {a}, and the opponent chose {b}!")
